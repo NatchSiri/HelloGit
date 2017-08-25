2f29cc44c86f385e8d95850bb8af8db7f206d3a5
Natch Sirisumpun <natchsiri@gmail.com>
2017-08-25 14:21:43 +0700

    Add Input rectangle and calculate area

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
 
 public class Main {
-
 	public static void main(String[] args) {
+		Scanner kb = new Scanner(System.in);
+		String name = "Natch Sirisumpun";
+		
 		System.out.println("Hello Git");
-		System.out.println("I am Natch Sirisumpun");
+		System.out.println("I am " + name);
+		
+		System.out.println("Enter rectangle width :");
+		int width = kb.nextInt();
+		System.out.println("Enter rectangle height :");
+		int height = kb.nextInt();
+		
+		System.out.println("The rectangle area is " + width*height);
 	}
 
 }
